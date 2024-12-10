bf3fb21786640ffbe7aacab526c07ab183bd9467
lhumphrey <humphrey.laura@gmail.com>
2024-12-10 18:27:53 -0500

    Partial Day 6 solution; going to try to refactor

--- Day_06/src/main.adb
+++ Day_06/src/main.adb
@@ -1,6 +1,5 @@
 with Ada.Text_IO; use Ada.Text_IO;
-with Ada.Containers.Vectors;
-with Ada.Containers.Hashed_Maps;
+with Ada.Containers.Ordered_Sets;
 with GNAT.Regpat; use GNAT.Regpat;
 
 procedure Main is
@@ -11,99 +10,151 @@ procedure Main is
 
    type Direction is (Up, Right, Down, Left);
 
+   package Direction_Sets is new
+     Ada.Containers.Ordered_Sets
+       (Element_Type => Direction);
+   use Direction_Sets;
+
    function Change_Direction (D: Direction) return Direction is
      (case D is when Up => Right, when Right => Down,
          when Down => Left, when Left => Up);
 
-   package Coordinate_Vectors is new
-     Ada.Containers.Vectors (Index_Type   => Positive,
-                             Element_Type => Coord);
-   use Coordinate_Vectors;
+   type Coord_Direction_Set_Array is array
+     (Coord'Range) of Direction_Sets.Set;
 
-   type Coordinate_Vector_Acc is access Coordinate_Vectors.Vector;
+   type Coord_2D_Direction_Set_Array is array
+     (Coord'Range) of Coord_Direction_Set_Array;
 
-   type Obstacle_Coord_Lists is
-     array (Positive range <>) of Coordinate_Vector_Acc;
+   type Coord_Obstacle_Array is array
+     (Coord'Range) of Boolean;
 
-   type Boolean_Array is array (Coord'Range) of Boolean;
-
-   type Boolean_Array_2D is array (Coord'Range) of Boolean_Array;
-
-   procedure Move_To_Closest_Obstacle
-     (Dir : in out Direction;
-      Obstacle_Coords_In_Row,
-      Obstacle_Coords_In_Col : Coordinate_Vectors.Vector;
-      X_Coord, Y_Coord : in out Extended_Coord;
-      Visited : in out Boolean_Array_2D)
-   is
-      New_Coord : Extended_Coord;
-   begin
-      case Dir is
-         when Down =>
-            New_Coord := Extended_Coord'Last;
-            for Obstacle_Coord of Obstacle_Coords_In_Col loop
-               if Obstacle_Coord > Y_Coord then
-                  New_Coord := Obstacle_Coord - 1;
-                  exit;
-               end if;
-            end loop;
-            for Y in Y_Coord .. Coord'Min (Coord'Last, New_Coord) loop
-               Visited (X_Coord) (Y) := True;
-            end loop;
-            Y_Coord := New_Coord;
-            Dir := Change_Direction (Dir);
-         when Right =>
-            New_Coord := Extended_Coord'Last;
-            for Obstacle_Coord of Obstacle_Coords_In_Row loop
-               if Obstacle_Coord > X_Coord then
-                  New_Coord := Obstacle_Coord - 1;
-                  exit;
-               end if;
-            end loop;
-            for X in X_Coord .. Coord'Min (Coord'Last, New_Coord) loop
-               Visited (X) (Y_Coord) := True;
-            end loop;
-            X_Coord := New_Coord;
-            Dir := Change_Direction (Dir);
-         when Up =>
-            New_Coord := 0;
-            for Obstacle_Coord of Obstacle_Coords_In_Col loop
-               if Obstacle_Coord < Y_Coord then
-                  New_Coord := Obstacle_Coord + 1;
-               end if;
-            end loop;
-            for Y in Coord'Max (Coord'First, New_Coord) .. Y_Coord loop
-               Visited (X_Coord) (Y) := True;
-            end loop;
-            Y_Coord := New_Coord;
-            Dir := Change_Direction (Dir);
-         when Left =>
-            New_Coord := 0;
-            for Obstacle_Coord of Obstacle_Coords_In_Row loop
-               if Obstacle_Coord < X_Coord then
-                  New_Coord := Obstacle_Coord + 1;
-               end if;
-            end loop;
-            for X in Coord'Max (Coord'First, New_Coord) .. X_Coord  loop
-               Visited (X) (Y_Coord) := True;
-            end loop;
-            X_Coord := New_Coord;
-            Dir := Change_Direction (Dir);
-      end case;
-   end Move_To_Closest_Obstacle;
+   type Coord_2D_Obstacle_Array is array
+     (Coord'Range) of Coord_Obstacle_Array;
 
    Filename : String := "input.txt";
    File : File_Type;
-   S : String (1 .. 131);
+   S : String (1 .. Extended_Coord'Last);
    S_Last : Positive;
    Row, Col : Positive := 1;
    Guard_Direction : Direction := Up;
-   Guard_X, Guard_Y : Extended_Coord := 0;
-   Obstacle_Coords_In_Row,
-   Obstacle_Coords_In_Col : Obstacle_Coord_Lists (Coord'Range);
-   Visited : Boolean_Array_2D := (others => (others => False));
+   Guard_Row_Start, Guard_Row, Guard_Col_Start, Guard_Col : Extended_Coord := 0;
+   Has_Obstacle : Coord_2D_Obstacle_Array := (others => (others => False));
+   Placed_Obstacle : Coord_2D_Obstacle_Array := (others => (others => False));
+   Directions_At_Coord : Coord_2D_Direction_Set_Array;
    Total1 : Natural := 0;
+   Obstacle_Count : Natural := 0;
    Draw : array (Coord'Range, Coord'Range) of Character := (others => (others => '.'));
+
+   procedure Move
+     (Row, Col : in out Extended_Coord;
+      Dir : in out Direction;
+      Directions : in out Coord_2D_Direction_Set_Array) is
+   begin
+      case Dir is
+         when Up =>
+            Directions (Row) (Col).Include (Up);
+            if Row - 1 in Coord'Range and then Has_Obstacle (Row - 1) (Col) then
+               Dir := Right;
+            else
+               Row := Row - 1;
+            end if;
+         when Right =>
+            Directions (Row) (Col).Include (Right);
+            if Col + 1 in Coord'Range and then Has_Obstacle (Row) (Col + 1) then
+               Dir := Down;
+            else
+               Col := Col + 1;
+            end if;
+         when Left =>
+            Directions (Row) (Col).Include (Left);
+            if Col - 1 in Coord'Range and then Has_Obstacle (Row) (Col - 1) then
+              Dir := Up;
+            else
+              Col := Col - 1;
+            end if;
+         when Down =>
+            Directions (Row) (Col).Include (Down);
+            if Row + 1 in Coord'Range and then Has_Obstacle (Row + 1) (Col) then
+               Dir := Left;
+            else
+               Row := Row + 1;
+            end if;
+      end case;
+   end Move;
+
+   function Has_Cycle
+     (Row, Col : in out Extended_Coord;
+      Dir : in out Direction;
+      Directions : in Coord_2D_Direction_Set_Array) return Boolean
+   is
+      Local_Directions := Directions;
+   begin
+      while Guard_Row in Coord'Range and Guard_Col in Coord'Range loop
+         Move (Row, Col, Dir, Local_Directions);
+         if not Empty (Directions (Row) (Col))
+           and then Directions (Row) (Col).Contains (Dir)
+         then
+            return True;
+         end if;
+      end if;
+      end loop;
+      return False;
+   end loop;
+   end Move;
+
+   function Can_Reach_Loop
+     (Row, Col : in Coord;
+      Dir : in Direction) return Boolean is
+   begin
+      if (Dir = Up and then Row = Coord'First)
+        or else (Dir = Down and then Row = Coord'Last)
+        or else (Dir = Left and then Col = Coord'First)
+        or else (Dir = Right and then Col = Coord'Last)
+      then
+         return False;
+      end if;
+
+      case Dir is
+         when Up =>
+            for C in Col + 1 .. Coord'Last - 1 loop
+               if Has_Obstacle (Row) (C) then
+                  return Can_Reach_Loop (Row, C - 1, Right);
+               elsif Directions_At_Coord (Row) (C).Contains (Right) then
+                  Placed_Obstacle (Row - 1) (Col) := True;
+                  return True;
+               end if;
+            end loop;
+         when Down =>
+            for C in reverse Coord'First + 1 .. Col - 1 loop
+               if Has_Obstacle (Row) (C) then
+                  return Can_Reach_Loop (Row, C + 1, Left);
+               elsif Directions_At_Coord (Row) (C).Contains (Left) then
+                  Placed_Obstacle (Row + 1) (Col) := True;
+                  return True;
+               end if;
+            end loop;
+         when Left =>
+            for R in reverse Coord'First + 1 .. Row - 1 loop
+               if Has_Obstacle (R) (Col) then
+                  return Can_Reach_Loop (R + 1, Col, Up);
+               elsif Directions_At_Coord (R) (Col).Contains (Up) then
+                  Placed_Obstacle (Row) (Col - 1) := True;
+                  return True;
+               end if;
+            end loop;
+         when Right =>
+            for R in Row + 1 .. Coord'Last - 1 loop
+               if Has_Obstacle (R) (Col) then
+                  return Can_Reach_Loop (R - 1, Col, Down);
+               elsif Directions_At_Coord (R) (Col).Contains (Down) then
+                  Placed_Obstacle (Row) (Col + 1) := True;
+                  return True;
+               end if;
+            end loop;
+      end case;
+
+      return False;
+   end Can_Reach_Loop;
 
 begin
 
@@ -114,19 +165,13 @@ begin
       Col := 1;
       while Col <= S_Last loop
          if S (Col) = '#' then
-            if Obstacle_Coords_In_Col (Col) = null then
-               Obstacle_Coords_In_Col (Col) := new Coordinate_Vectors.Vector;
-            end if;
-            if Obstacle_Coords_In_Row (Row) = null then
-               Obstacle_Coords_In_Row (Row) := new Coordinate_Vectors.Vector;
-            end if;
-            Obstacle_Coords_In_Col (Col).Append (Row);
-            Obstacle_Coords_In_Row (Row).Append (Col);
-            Draw (Row, Col) := '#';
+            Has_Obstacle (Row) (Col) := True;
          elsif S (Col) = '^' then
-            Guard_X := Col;
-            Guard_Y := Row;
-            Draw (Row, Col) := '^';
+            Guard_Col_Start := Col;
+            Guard_Col := Guard_Col_Start;
+            Guard_Row_Start := Row;
+            Guard_Row := Guard_Row_Start;
+            Directions_At_Coord (Guard_Row) (Guard_Col).Include (Up);
          end if;
          Col := Col + 1;
       end loop;
@@ -135,18 +180,95 @@ begin
 
    Close (File);
 
-   while Guard_X in Coord'Range and Guard_Y in Coord'Range loop
-      Move_To_Closest_Obstacle (Guard_Direction,
-                                Obstacle_Coords_In_Row (Guard_Y).all,
-                                Obstacle_Coords_In_Col (Guard_X).all,
-                                Guard_X,
-                                Guard_Y,
-                                Visited);
+   while Guard_Row in Coord'Range and Guard_Col in Coord'Range loop
+      Move (Guard_Row, Guard_Col, Guard_Direction);
    end loop;
 
-   for I in Visited'Range loop
-      for J in Visited (I)'Range loop
-         if Visited (I) (J) then
+   Guard_Row := Guard_Row_Start;
+   Guard_Col := Guard_Col_Start;
+   Guard_Direction := Up;
+
+   --  while Guard_Row in Coord'Range and Guard_Col in Coord'Range loop
+   --     case Guard_Direction is
+   --        when Up =>
+   --           Directions_At_Coord (Guard_Row) (Guard_Col).Include (Up);
+   --           if Guard_Row - 1 in Coord'Range and then
+   --             Has_Obstacle (Guard_Row - 1) (Guard_Col)
+   --           then
+   --             Guard_Direction := Right;
+   --           else
+   --              if Can_Reach_Loop (Guard_Row, Guard_Col, Guard_Direction) then
+   --                 Obstacle_Count := Obstacle_Count + 1;
+   --              end if;
+   --              Guard_Row := Guard_Row - 1;
+   --           end if;
+   --        when Right =>
+   --           Directions_At_Coord (Guard_Row) (Guard_Col).Include (Right);
+   --           if Guard_Col + 1 in Coord'Range and then
+   --             Has_Obstacle (Guard_Row) (Guard_Col + 1)
+   --           then
+   --             Guard_Direction := Down;
+   --           else
+   --              if Can_Reach_Loop (Guard_Row, Guard_Col, Guard_Direction) then
+   --                 Obstacle_Count := Obstacle_Count + 1;
+   --              end if;
+   --
+   --              Guard_Col := Guard_Col + 1;
+   --           end if;
+   --        when Left =>
+   --           Directions_At_Coord (Guard_Row) (Guard_Col).Include (Left);
+   --           if Guard_Col - 1 in Coord'Range and then
+   --             Has_Obstacle (Guard_Row) (Guard_Col - 1)
+   --           then
+   --             Guard_Direction := Up;
+   --           else
+   --              if Can_Reach_Loop (Guard_Row, Guard_Col, Guard_Direction) then
+   --                 Obstacle_Count := Obstacle_Count + 1;
+   --              end if;
+   --              Guard_Col := Guard_Col - 1;
+   --           end if;
+   --        when Down =>
+   --           Directions_At_Coord (Guard_Row) (Guard_Col).Include (Down);
+   --           if Guard_Row + 1 in Coord'Range and then
+   --             Has_Obstacle (Guard_Row + 1) (Guard_Col)
+   --           then
+   --             Guard_Direction := Left;
+   --           else
+   --              if Can_Reach_Loop (Guard_Row, Guard_Col, Guard_Direction) then
+   --                 Obstacle_Count := Obstacle_Count + 1;
+   --              end if;
+   --              Guard_Row := Guard_Row + 1;
+   --           end if;
+   --     end case;
+   --     Put_Line ("Obstacle_Count : " & Obstacle_Count'Image);
+   --  end loop;
+
+
+
+   --  for I in Directions_At_Coord'Range loop
+   --     for J in Directions_At_Coord (I)'Range loop
+   --        if Placed_Obstacle (I) (J) then
+   --           Put ("+");
+   --        elsif Has_Obstacle (I) (J) then
+   --           Put ("#");
+   --        elsif Directions_At_Coord (I) (J).Contains (Up) then
+   --           Put ("U");
+   --        elsif Directions_At_Coord (I) (J).Contains (Down) then
+   --           Put ("D");
+   --        elsif Directions_At_Coord (I) (J).Contains (Left) then
+   --           Put ("L");
+   --        elsif Directions_At_Coord (I) (J).Contains (Right) then
+   --           Put ("R");
+   --        else
+   --           Put (".");
+   --        end if;
+   --     end loop;
+   --     New_Line;
+   --  end loop;
+
+   for I in Directions_At_Coord'Range loop
+      for J in Directions_At_Coord (I)'Range loop
+         if Integer (Directions_At_Coord (I) (J).Length) > 0 then
             Total1 := Total1 + 1;
          end if;
       end loop;
@@ -154,4 +276,7 @@ begin
 
    Put_Line ("Unique Positions Visited: " & Total1'Image);
 
+   Put_Line ("Obstacles that can be placed: " & Obstacle_Count'Image);
+   -- 437 is too low
+   -- Derek's code gives 1711
 end;
