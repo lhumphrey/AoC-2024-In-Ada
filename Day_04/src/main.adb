fb9732a25f281d4d85a3c8a91befb2b23da98616
lhumphrey <humphrey.laura@gmail.com>
2024-12-31 15:34:25 -0500

    Day04 in Ada cleanup and added flexibiliy in input size

--- Day_04/src/main.adb
+++ Day_04/src/main.adb
@@ -1,14 +1,12 @@
 with Ada.Text_IO; use Ada.Text_IO;
 
--- Ada.Text_IO.Skip_Line
-
-procedure Main with SPARK_Mode is
+procedure Main is
 
    Filename : String := "input.txt";
    File : File_Type;
-   S : String (1 .. 141);
-   A : array (1 .. 140, 1 .. 141) of Character := (others => (others => ' '));
-   Line_Num, Last_Idx : Natural := 0;
+   S : String (1 .. 256);
+   A : array (1 .. 256, 1 .. 256) of Character := (others => (others => ' '));
+   Last_Row, Last_Col : Natural := 0;
    Total1, Total2 : Natural := 0;
 
 begin
@@ -16,17 +14,17 @@ begin
    Open (File, In_File, Filename);
 
    while not End_Of_File (File) loop
-      Get_Line (File, S, Last_Idx);
-      Line_Num := Line_Num + 1;
-      for I in 1 .. Last_Idx loop
-         A (Line_Num, I) := S (I);
+      Get_Line (File, S, Last_Col);
+      Last_Row := Last_Row + 1;
+      for I in 1 .. Last_Col loop
+         A (Last_Row, I) := S (I);
       end loop;
    end loop;
 
    Close (File);
 
-   for I in A'First(1) .. A'Last(1) loop
-      for J in A'First(2) .. A'Last(2) loop
+   for I in A'First(1) .. Last_Row loop
+      for J in A'First(2) .. Last_Col loop
          -- Down
          Total1 := Total1 +
            (if I <= A'Last(1) - 3
@@ -88,8 +86,8 @@ begin
 
    Put_Line ("Total 'XMAS's: " & Total1'Image);
 
-   for I in A'First(1) .. A'Last(1) loop
-      for J in A'First(2) .. A'Last(2) loop
+   for I in A'First(1) .. Last_Row loop
+      for J in A'First(2) .. Last_Col loop
          Total2 := Total2 +
            (if A(I, J) = 'A'
             and then I <= A'Last(1) - 1 and then J <= A'Last(2) - 1
